b8fe14598a04bd99f6831deb60ddf1756fc44685
aries1711 <levfreey17@gmail.com>
2020-09-05 11:35:49 +0700

    cicilan halaman daftar

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/SignUpActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.inddevid.aksiberbagi_donatur.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.inddevid.aksiberbagi_donatur.R
 
@@ -11,5 +13,12 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_activity)
         supportActionBar?.hide()
+        val backButton: Button = findViewById(R.id.closeDaftar)
+        backButton.setOnClickListener{ redirectBack() }
+    }
+
+    private fun redirectBack(){
+        val intent = Intent (this@SignUpActivity, IntroActivity::class.java)
+        startActivity(intent)
     }
 }
