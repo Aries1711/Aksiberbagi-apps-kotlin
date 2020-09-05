50aece03e6d0cbfb9200ca86f94bfc8f4fdbed2b
aries1711 <levfreey17@gmail.com>
2020-09-05 10:46:57 +0700

    fixing tampilan

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/SignUpActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/SignUpActivity.kt
@@ -1,10 +1,12 @@
 package com.inddevid.aksiberbagi_donatur.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.inddevid.aksiberbagi_donatur.R
 
 class SignUpActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_activity)
