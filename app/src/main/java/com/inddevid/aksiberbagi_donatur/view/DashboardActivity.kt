7218ab55c3b978fa068d4518cc40576363dca726
aries1711 <levfreey17@gmail.com>
2020-09-08 13:22:39 +0700

    navigation bottom bar

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DashboardActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DashboardActivity.kt
@@ -1,12 +1,16 @@
 package com.inddevid.aksiberbagi_donatur.view
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.inddevid.aksiberbagi_donatur.R
+
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard_activity)
     }
+
 }
