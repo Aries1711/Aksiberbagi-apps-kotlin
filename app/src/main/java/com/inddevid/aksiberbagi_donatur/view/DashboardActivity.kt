7967ba71a9838e43b121f7fbe6493392650e91aa
aries1711 <levfreey17@gmail.com>
2020-09-11 02:51:19 +0700

    benerin actionBar besok dan controll tiap activitynya

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DashboardActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DashboardActivity.kt
@@ -2,7 +2,10 @@ package com.inddevid.aksiberbagi_donatur.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.inddevid.aksiberbagi_donatur.R
 
 
@@ -10,8 +13,11 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard_activity)
-        val appBar: Toolbar = findViewById(R.id.upAppbar)
-        setSupportActionBar(appBar)
+        val bottomBar: BottomAppBar = findViewById(R.id.bottom_bar)
+        bottomBar.bringToFront()
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavAksiberbagi)
+        val navController = findNavController(R.id.fragment)
+        bottomNav.setupWithNavController(navController)
     }
 
 
