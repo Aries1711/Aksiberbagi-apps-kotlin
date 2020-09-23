a920dc2da3c94c5eeab65d89dffe8e22ae475386
aries1711 <levfreey17@gmail.com>
2020-09-24 02:22:09 +0700

    donasi rutin btn gabung top done

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/BerandaIndex.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/BerandaIndex.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.inddevid.aksiberbagi_donatur.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,7 +40,17 @@ class BerandaIndex : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda_index, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_beranda_index , container, false)
+
+        val imageDonasiRutin : ImageView = view.findViewById(R.id.donasiRutinLogo)
+        Glide.with(requireActivity()).load(R.drawable.donasi_rutin_icon).into(imageDonasiRutin)
+
+        val btnGbgDonRut: CardView = view.findViewById(R.id.cardGabungDonasiRutin)
+        btnGbgDonRut.setOnClickListener(View.OnClickListener {
+            Toast.makeText(requireActivity(), "Gabung Donasi Rutin ?", Toast.LENGTH_LONG).show()
+        })
+
+        return view
     }
 
     companion object {
