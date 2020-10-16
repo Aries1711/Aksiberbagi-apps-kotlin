10c2deea700bde1921665cb4af8b548f6e93ba53
aries1711 <levfreey17@gmail.com>
2020-10-16 17:37:04 +0700

    perhatian lanjut detail donasi

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DonasiRutinActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/DonasiRutinActivity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.LinearLayout
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.inddevid.aksiberbagi_donatur.R
@@ -17,7 +18,7 @@ class DonasiRutinActivity : AppCompatActivity() {
         setContentView(R.layout.donasi_rutin_activity)
         val toolbar: Toolbar = findViewById(R.id.upAppbarDonasiRutin)
         toolbar.title = "Donasi Rutin"
-        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitleTextColor(Color.WHITE)
         toolbar.setNavigationOnClickListener{
             val mIntent = Intent(this, DashboardActivity::class.java)
             val mBundle = Bundle()
@@ -25,40 +26,54 @@ class DonasiRutinActivity : AppCompatActivity() {
             mIntent.putExtras(mBundle)
             startActivity(mIntent)}
 
-        // set pilihan program dropdown
+//         set pilihan program dropdown
         val items = listOf("Wujudkan Pondok Pesantren Tahfidz Qur’an Hadist Internasional Pekanbaru, Riau", "Simpan Hartamu dilangit, Sedekah Jariyah Atas Nama Keluarga", "Bangun Rumah di Surga: Sedekah Jariah Renovasi Masjid Pelosok", "Android")
         val adapterProgram = ArrayAdapter(this, R.layout.list_pilih_program_dropdown, items)
-        val dropDownProgram : AutoCompleteTextView = findViewById(R.id.pilihProgram)
-        dropDownProgram.setAdapter(adapterProgram)
+        val dropDownProgram : Spinner = findViewById(R.id.spinerProgram)
+        dropDownProgram.adapter = adapterProgram
 
         val frekuensiItems = listOf("Harian", "Mingguan", "Bulanan")
         val adapterFrekuensi = ArrayAdapter(this, R.layout.list_pilih_program_dropdown, frekuensiItems)
-        val dropDownFrekuensi : AutoCompleteTextView = findViewById(R.id.pilihFrekuensi)
-        dropDownFrekuensi.setAdapter(adapterFrekuensi)
+        val dropDownFrekuensi : Spinner = findViewById(R.id.spinerFrekuensi)
+        dropDownFrekuensi.adapter = adapterFrekuensi
 
-        //set val linearLayout untuk frekuensi yang dipilih
-        var layoutMingguan: LinearLayout = findViewById(R.id.layoutFrekuensiMingguan)
-        var layoutBulanan: LinearLayout = findViewById(R.id.layoutFrekuensiBulanan)
+        //set val spinner untuk frekuensi yang dipilih
+        val frekuensiMingguans = listOf("senin", "selasa", "rabu", "kamis", "jumat", "minggu")
+        val dropDownMingguan : Spinner = findViewById(R.id.frekuensiMingguan)
+        val adapterMingguan = ArrayAdapter(this, R.layout.list_pilih_program_dropdown, frekuensiMingguans)
+        dropDownMingguan.adapter = adapterMingguan
 
-//        dropDownFrekuensi.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-//            override fun onItemSelected(p0: AdapterView<frekuensiItems>?, p1: View?, p2: Int, p3: Long) {
-////                if (frekuensiItems[p2] == "Mingguan"){
-////                    show(layoutMingguan)
-////                }else if(frekuensiItems[p2] == "Bulanan"){
-////                    show(layoutBulanan)
-////                }else{
-////                    //
-////                }
-//                Toast.makeText(this@DonasiRutinActivity, "Bisa", Toast.LENGTH_LONG).show()
-//            }
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//                //
-//            }
-//
-//        }
+        val frekuensiBulanans = listOf("1","2","3","5","6","7","8","9","10", "11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28")
+        val dropDownBulanan : Spinner = findViewById(R.id.frekuensiBulanan)
+        val adapterBulanan = ArrayAdapter(this, R.layout.list_pilih_program_dropdown,frekuensiBulanans)
+        dropDownBulanan.adapter = adapterBulanan
+
+        //set layout hidden untuk frekuensi yang dipilih
+        val layoutB : LinearLayout = findViewById(R.id.layoutBulanan)
+        val layoutM : LinearLayout = findViewById(R.id.layoutMingguan)
+
+        dropDownFrekuensi.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (frekuensiItems[p2] == "Mingguan"){
+                    show(layoutM)
+                    gone(layoutB)
+                }else if (frekuensiItems[p2] == "Bulanan"){
+                    gone(layoutM)
+                    show(layoutB)
+                }else{
+                    gone(layoutM)
+                    gone(layoutB)
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
-    fun hidden(view: View){
-        view.visibility = View.INVISIBLE
+    fun gone(view: View){
+        view.visibility = View.GONE
     }
 
     fun show(view: View){
--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/ProgramDetailActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/ProgramDetailActivity.kt
@@ -2,7 +2,9 @@ package com.inddevid.aksiberbagi_donatur.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.inddevid.aksiberbagi_donatur.R
 import com.inddevid.aksiberbagi_donatur.model.ListDonasi
 import com.inddevid.aksiberbagi_donatur.presenter.ListDonasiAdapter
@@ -31,12 +34,12 @@ class ProgramDetailActivity : AppCompatActivity() {
         val pDonaturB:String = ""
         val tDonatur:String = "13 jam yang lalu"
         val arrayDonasi = ArrayList<ListDonasi>()
-        arrayDonasi.add(ListDonasi(imagDonatur,nmDonatur,donDonatur,pDonaturA,tDonatur))
-        arrayDonasi.add(ListDonasi(imagDonatur,nmDonatur,donDonatur,pDonaturB,tDonatur))
-        arrayDonasi.add(ListDonasi(imagDonatur,nmDonatur,donDonatur,pDonaturA,tDonatur))
-        arrayDonasi.add(ListDonasi(imagDonatur,nmDonatur,donDonatur,pDonaturB,tDonatur))
-        arrayDonasi.add(ListDonasi(imagDonatur,nmDonatur,donDonatur,pDonaturB,tDonatur))
-        val myAdapterListDonasi = ListDonasiAdapter(arrayDonasi,this)
+        arrayDonasi.add(ListDonasi(imagDonatur, nmDonatur, donDonatur, pDonaturA, tDonatur))
+        arrayDonasi.add(ListDonasi(imagDonatur, nmDonatur, donDonatur, pDonaturB, tDonatur))
+        arrayDonasi.add(ListDonasi(imagDonatur, nmDonatur, donDonatur, pDonaturA, tDonatur))
+        arrayDonasi.add(ListDonasi(imagDonatur, nmDonatur, donDonatur, pDonaturB, tDonatur))
+        arrayDonasi.add(ListDonasi(imagDonatur, nmDonatur, donDonatur, pDonaturB, tDonatur))
+        val myAdapterListDonasi = ListDonasiAdapter(arrayDonasi, this)
 
         // inflate the vertical total Donasi
         var mainMenuAll = findViewById<RecyclerView>(R.id.totalDonasiRecycler)
@@ -53,9 +56,26 @@ class ProgramDetailActivity : AppCompatActivity() {
         Glide.with(this).load(imageUrl).apply(options).into(imageProgram)
 
         val toolbar: Toolbar = findViewById(R.id.upAppbarProgramDetail)
-        toolbar.setNavigationOnClickListener{startActivity(Intent(this@ProgramDetailActivity, DashboardActivity::class.java))}
+        toolbar.setNavigationOnClickListener{startActivity(
+            Intent(
+                this@ProgramDetailActivity,
+                DashboardActivity::class.java
+            )
+        )}
         val myWebView: WebView = findViewById(R.id.keteranganProgram)
         myWebView.loadUrl("https://aksiberbagi.com/sedekahair")
+
+        //deklarasi btn Donasi dan dialog swipe
+        val btnDonasi: Button = findViewById(R.id.donasiBtn)
+        val dialogPembayaran = BottomSheetDialog(this)
+        val view : View  = layoutInflater.inflate(R.layout.dialog_pembayaran_donasi, null)
+        dialogPembayaran.dismiss()
+
+        dialogPembayaran.setContentView(view)
+        btnDonasi.setOnClickListener {
+            dialogPembayaran.show()
+        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
