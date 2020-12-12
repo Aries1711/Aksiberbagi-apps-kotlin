e2ce8acd9b5eb313c5d0ef1d637c89e16d6d07d6
aries1711 <levfreey17@gmail.com>
2020-12-12 20:50:56 +0700

    oke zakat lanjut setelah flashsale

--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/KalkulatorActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/KalkulatorActivity.kt
@@ -2,7 +2,9 @@ package com.inddevid.aksiberbagi_donatur.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.inddevid.aksiberbagi_donatur.R
@@ -18,12 +20,48 @@ class KalkulatorActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener{ startActivity(Intent(this@KalkulatorActivity, ZakatActivity::class.java))}
 
         val btnKalkulatorPenghasilan : FrameLayout = findViewById(R.id.btnKalkulatorPenghasilan)
+        val layoutPenghasilan : LinearLayout = findViewById(R.id.kalkulatorPenghasilan)
+
         val btnKalkulatorPerdagangan : FrameLayout = findViewById(R.id.btnKalkulatorPerdagangan)
+        val layoutPerdagangan : LinearLayout = findViewById(R.id.kalkulatorPerdagangan)
+
         val btnKalkulatorEmas : FrameLayout = findViewById(R.id.btnKalkulatorEmas)
+        val layoutEmas : LinearLayout = findViewById(R.id.kalkulatorEmas)
+
         val btnKalkulatorTabungan : FrameLayout = findViewById(R.id.btnKalkulatorTabungan)
+        val layoutTabungan : LinearLayout = findViewById(R.id.kalkulatorTabungan)
+
+        layoutPenghasilan.visibility = View.VISIBLE
 
         btnKalkulatorPenghasilan.setOnClickListener {
-
+            layoutPenghasilan.visibility = View.VISIBLE
+            layoutPerdagangan.visibility = View.GONE
+            layoutEmas.visibility = View.GONE
+            layoutTabungan.visibility = View.GONE
         }
+
+        btnKalkulatorPerdagangan.setOnClickListener {
+            layoutPerdagangan.visibility = View.VISIBLE
+            layoutPenghasilan.visibility = View.GONE
+            layoutEmas.visibility = View.GONE
+            layoutTabungan.visibility = View.GONE
+        }
+
+        btnKalkulatorEmas.setOnClickListener {
+            layoutEmas.visibility = View.VISIBLE
+            layoutPenghasilan.visibility = View.GONE
+            layoutPerdagangan.visibility = View.GONE
+            layoutTabungan.visibility = View.GONE
+        }
+
+        btnKalkulatorTabungan.setOnClickListener {
+            layoutTabungan.visibility = View.VISIBLE
+            layoutPenghasilan.visibility = View.GONE
+            layoutEmas.visibility = View.GONE
+            layoutPerdagangan.visibility = View.GONE
+        }
+
+
+
     }
 }
--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/ZakatActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/ZakatActivity.kt
@@ -2,15 +2,19 @@ package com.inddevid.aksiberbagi_donatur.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.inddevid.aksiberbagi_donatur.R
 
 class ZakatActivity : AppCompatActivity() {
+    private var idProgramZakat : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.zakat_activity)
@@ -31,8 +35,12 @@ class ZakatActivity : AppCompatActivity() {
         val btnBayarZakatHeader: LinearLayout = findViewById(R.id.btnZakatBayar)
         val btnZakatKalkulator: LinearLayout = findViewById(R.id.btnZakatHitung)
 
-        btnBayarZakatHeader.setOnClickListener {
+        val dialogPembayaran = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+        val view : View = layoutInflater.inflate(R.layout.dialog_donasi_zakat, null)
+        dialogPembayaran.setContentView(view)
 
+        btnBayarZakatHeader.setOnClickListener {
+            dialogPembayaran.show()
         }
 
         btnZakatKalkulator.setOnClickListener {
@@ -43,14 +51,36 @@ class ZakatActivity : AppCompatActivity() {
         //list pilihan zakat berdasarkan jenis
         val imageZakatTabungan: ImageView = findViewById(R.id.imageZakatTabungan)
         Glide.with(this).load("https://aksiberbagi.com/storage/program/2a2692b30977847c99b23f478aab9e02_zsimpan.jpg").apply(options).into(imageZakatTabungan)
+        val btnZakatTabungan: Button = findViewById(R.id.btnZakatTabungan)
+        btnZakatTabungan.setOnClickListener {
+            dialogPembayaran.show()
+            idProgramZakat = 150
+        }
+
 
         val imageZakatEmas: ImageView = findViewById(R.id.imageZakatEmas)
         Glide.with(this).load("https://aksiberbagi.com/storage/program/cbabdc05e9c3b9e802e1c028c42470b9_zemas.jpg").apply(options).into(imageZakatEmas)
+        val btnZakatEmas: Button = findViewById(R.id.btnZakatEmas)
+        btnZakatEmas.setOnClickListener {
+            dialogPembayaran.show()
+            idProgramZakat = 152
+        }
 
         val imageZakatDagang: ImageView = findViewById(R.id.imageZakatPerdagangan)
         Glide.with(this).load("https://aksiberbagi.com/storage/program/0ee1c6f32d6d11f44cbb9ffb42c3ed84_zdagang.jpg").apply(options).into(imageZakatDagang)
+        val btnZakatPerdagangan : Button = findViewById(R.id.btnZakatPerdagangan)
+        btnZakatPerdagangan.setOnClickListener {
+            dialogPembayaran.show()
+            idProgramZakat = 151
+        }
 
         val imageZakatPenghasilan: ImageView = findViewById(R.id.imageZakatPenghasilan)
         Glide.with(this).load("https://aksiberbagi.com/storage/program/04fb0a8e0d93c42586fc31f186c07bde_zhasilan.jpg").apply(options).into(imageZakatPenghasilan)
+        val btnZakatPenghasilan : Button = findViewById(R.id.btnZakatPenghasilan)
+        btnZakatPenghasilan.setOnClickListener {
+            dialogPembayaran.show()
+            idProgramZakat = 153
+        }
+
     }
 }
