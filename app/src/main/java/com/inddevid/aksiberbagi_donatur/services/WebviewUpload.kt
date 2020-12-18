40b1ec2904f7079e08ce33834f97589b2a30e492
aries1711 <levfreey17@gmail.com>
2020-12-19 00:41:13 +0700

    oke publik ajukan done

---
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/services/WebviewUpload.kt
@@ -0,0 +1,36 @@
+package com.inddevid.aksiberbagi_donatur.services
+
+import android.content.ActivityNotFoundException
+import android.net.Uri
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.Toast
+import com.inddevid.aksiberbagi_donatur.view.WebviewAjuanActivity
+
+
+open class WebviewUpload : WebChromeClient() {
+    // reference to activity instance. May be unnecessary if your web chrome client is member class.
+    private val myActivity: WebviewAjuanActivity? = null
+    override fun onShowFileChooser(
+        webView: WebView,
+        filePathCallback: ValueCallback<Array<Uri>>,
+        fileChooserParams: FileChooserParams
+    ): Boolean {
+        // make sure there is no existing message
+        if (myActivity!!.uploadMessage != null) {
+            myActivity.uploadMessage!!.onReceiveValue(null)
+            myActivity.uploadMessage = null
+        }
+        myActivity.uploadMessage = filePathCallback
+        val intent = fileChooserParams.createIntent()
+        try {
+            myActivity.startActivityForResult(intent, myActivity.REQUEST_SELECT_FILE)
+        } catch (e: ActivityNotFoundException) {
+            myActivity.uploadMessage = null
+            Toast.makeText(myActivity, "Cannot open file chooser", Toast.LENGTH_LONG).show()
+            return false
+        }
+        return true
+    }
+}
--- app/src/main/java/com/inddevid/aksiberbagi_donatur/view/WebviewAjuanActivity.kt
+++ app/src/main/java/com/inddevid/aksiberbagi_donatur/view/WebviewAjuanActivity.kt
@@ -5,16 +5,33 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.inddevid.aksiberbagi_donatur.R
+import com.inddevid.aksiberbagi_donatur.services.WebviewUpload
+
 
 class WebviewAjuanActivity : AppCompatActivity() {
     private val TAG = "Webview Publik ajuan"
     private val  USER_AGENT = "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36"
+    val REQUEST_SELECT_FILE = 100
+    var uploadMessage: ValueCallback<Array<Uri>>? = null
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_SELECT_FILE) {
+            if (uploadMessage == null) return;
+            uploadMessage!!.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+            uploadMessage = null;
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +52,29 @@ class WebviewAjuanActivity : AppCompatActivity() {
         webView.settings.useWideViewPort = true
         webView.settings.loadWithOverviewMode = true
         webView.scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+        webView.webChromeClient = object: WebviewUpload() {
+            override fun onShowFileChooser(
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                // make sure there is no existing message
+                if (this@WebviewAjuanActivity.uploadMessage != null) {
+                    this@WebviewAjuanActivity.uploadMessage!!.onReceiveValue(null)
+                    this@WebviewAjuanActivity.uploadMessage = null
+                }
+                this@WebviewAjuanActivity.uploadMessage = filePathCallback
+                val intent = fileChooserParams.createIntent()
+                try {
+                    this@WebviewAjuanActivity.startActivityForResult(intent, this@WebviewAjuanActivity.REQUEST_SELECT_FILE)
+                } catch (e: ActivityNotFoundException) {
+                    this@WebviewAjuanActivity.uploadMessage = null
+                    Toast.makeText(this@WebviewAjuanActivity, "Cannot open file chooser", Toast.LENGTH_LONG).show()
+                    return false
+                }
+                return true
+            }
+        }
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 view.loadUrl(url)
@@ -55,13 +95,19 @@ class WebviewAjuanActivity : AppCompatActivity() {
                 Log.e(TAG, "UrlError: $failingUrl")
                 val uri = Uri.parse(failingUrl)
                 val intent = Intent(Intent.ACTION_VIEW, uri)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                intent.addFlags(
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or
+                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                )
                 try {
                     startActivity(intent)
                 } catch (e: ActivityNotFoundException) {
-                    val toast = Toast.makeText(this@WebviewAjuanActivity,"Mohon install Aplikasi pembayaran pihak ketiga terlebih dahulu", Toast.LENGTH_LONG)
+                    val toast = Toast.makeText(
+                        this@WebviewAjuanActivity,
+                        "Mohon install Aplikasi pembayaran pihak ketiga terlebih dahulu",
+                        Toast.LENGTH_LONG
+                    )
                     toast.show()
                 }
             }
